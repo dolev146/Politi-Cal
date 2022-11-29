50a77e34039a065cecc5801ca5b032f4ee135649
Ofri Tavor <ofritavor55@gmail.com>
2022-11-29 12:25:38 +0200

    Finished creating the celeb model

--- app/src/main/java/com/example/politi_cal/models/Celeb.kt
+++ app/src/main/java/com/example/politi_cal/models/Celeb.kt
@@ -1,3 +1,16 @@
 package com.example.politi_cal.models
 
-data class Celeb(val uid: String = "")
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+
+data class Celeb(
+    val celebID: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    var birthDate: LocalDate = LocalDate.parse("2022-01-01"),
+    val celebDesc: String = "",
+    val imageURL: String = "",
+    val companyID: String = "",
+    val interestID: String = "",
+    val lastUpdate: LocalDateTime = LocalDateTime.now()
+    )
