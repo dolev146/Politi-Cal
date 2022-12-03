0df51fd812515b1e324165a102282834c5ac48dc
Ofri Tavor <ofritavor55@gmail.com>
2022-12-03 20:34:59 +0200

    Added listboxes and switches to analytics

---
+++ app/src/main/java/com/example/politi_cal/screens/analytics/PieChartData.kt
@@ -0,0 +1,11 @@
+package com.example.politi_cal.screens.analytics
+
+data class PieChartData(
+    var Vote:  String?,
+    var value: Float?
+)
+
+val getPieChartData = listOf(
+    PieChartData("Left", 45.6F),
+    PieChartData("Right", 54.4F)
+)
